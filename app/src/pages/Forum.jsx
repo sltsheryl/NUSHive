4c171809a33aa9944d118ea21eb3fd8504b407de
sltsheryl <sltsheryl@gmail.com>
2023-01-14 13:07:33 +0800

    Add home page

---
+++ app/src/pages/Forum.jsx
@@ -0,0 +1,9 @@
+const Forum = () => {
+    return (
+        <div>
+            <h1>This is the Forum Page</h1>
+        </div>
+    )
+}
+
+export default Forum;
---
+++ app/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import NavBar from '../components/NavBar'
+const Home = () => {
+    return (
+        <div>
+            <NavBar />
+            <h1>This is the Home Page</h1>
+        </div>
+    )
+}
+
+export default Home;
